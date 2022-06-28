bc0b52257d634cc7cf4522cfb79ad0b1436487b6
Mohammad alhallaq <mohammad.alhallaq1@gmail.com>
2022-06-28 14:48:59 +0300

    few tweaks!

--- resources/js/pages/auth/login.vue
+++ resources/js/pages/auth/login.vue
@@ -1,39 +1,82 @@
 <template>
+    <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-lg mx-auto">
+            <form action="" class="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl">
+                <div>
+                    <label for="email" class="text-sm font-medium">Email</label>
 
-    <div class="h-screen flex flex-col justify-center items-center">
-        <div class="w-full max-w-xs">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="submit">
-                <div v-if="errors.credentials"
-                     class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mb-4" role="alert">
-                    <p class="font-bold">Heads up</p>
-                    <p>{{ errors.credentials }}</p>
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
-                    </label>
-                    <input v-model="form.username" :class="{ 'border-red-500': errors.username }"
-                           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-3 focus:outline-none focus:shadow-outline"
-                           id="username" type="text">
-                    <p v-if="errors.username" class="text-red-500 text-xs italic">Please choose a password.</p>
+                    <div class="relative mt-1">
+                        <input
+                            type="email"
+                            id="email"
+                            class="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                            placeholder="Enter email"
+                        />
 
+                        <span class="absolute inset-y-0 inline-flex items-center right-4">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+              <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+              />
+            </svg>
+          </span>
+                    </div>
                 </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input v-model="form.password" :class="{ 'border-red-500': errors.password }"
-                           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                           id="password" type="password">
-                    <p v-if="errors.password" class="text-red-500 text-xs italic">Please choose a password.</p>
+
+                <div>
+                    <label for="password" class="text-sm font-medium">Password</label>
+
+                    <div class="relative mt-1">
+                        <input
+                            type="password"
+                            id="password"
+                            class="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+                            placeholder="Enter password"
+                        />
+
+                        <span class="absolute inset-y-0 inline-flex items-center right-4">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+              <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              />
+            </svg>
+          </span>
+                    </div>
                 </div>
-                <div class="flex items-center justify-between">
-                    <button
-                        class="w-full bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                        type="submit">
-                        Sign In
-                    </button>
-                </div>
+
+                <button type="submit"
+                        class="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg">
+                    Sign in
+                </button>
+
+                <p class="text-sm text-center text-gray-500">
+                    No account?
+                    <a class="underline" href="">Sign up</a>
+                </p>
             </form>
         </div>
     </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    @include('partials.header')
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
-<body class="g-sidenav-show bg-gray-200">
+<body>
 @inertia
 @routes
 </body>
